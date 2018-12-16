ce227bf8c643d1adc7b1d4c661d1390e5dd69dc9
hrbrmstr <bob@rud.is>
2018-12-16 13:54:38 -0500

    R package repo initialization complete

---
+++ R/htmlunit-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/htmlunit>
+#' - BugReports: <https://gitlab.com/hrbrmstr/htmlunit/issues>
+#' 
+#' @md
+#' @name htmlunit
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
